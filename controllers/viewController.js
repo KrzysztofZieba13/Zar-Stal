94f137d5e22575b5e45ec29c51fba529f057c246
Krzysztof <drkrzysztofzieba@gmail.com>
2024-07-01 20:41:24 +0200

    adding realizations elements

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,6 +1,14 @@
 exports.getOverview = async (req, res) => {
   try {
-    res.status(200).render('strona');
+    res.status(200).render('index');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.getRealizations = async (req, res) => {
+  try {
+    res.status(200).render('realizations');
   } catch (err) {
     console.log(err);
   }
